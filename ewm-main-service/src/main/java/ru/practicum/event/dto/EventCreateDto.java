4c712acbda3fa0a151f1db5c719b561a9648c0d6
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-09-17 16:56:07 +0300

    fix: исправлены размеры строковых полей событий (+ добавлены проверки в dto).

--- ewm-main-service/src/main/java/ru/practicum/event/dto/EventCreateDto.java
+++ ewm-main-service/src/main/java/ru/practicum/event/dto/EventCreateDto.java
@@ -1,16 +1,20 @@
 package ru.practicum.event.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import lombok.extern.jackson.Jacksonized;
+import org.hibernate.validator.constraints.Length;
 import ru.practicum.event.model.Location;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+
+import static ru.practicum.common.Utils.DATE_PARAM_FORMAT_PATTERN;
 
 @SuperBuilder
 @Jacksonized
@@ -19,15 +23,19 @@ import java.time.LocalDateTime;
 @AllArgsConstructor
 public class EventCreateDto {
     @NotBlank
+    @Length(min = 20, max = 2000)
     private String annotation;
     @NotBlank
+    @Length(min = 3, max = 120)
     private String title;
     @NotBlank
+    @Length(min = 20, max = 7000)
     private String description;
     @NotNull
     @JsonProperty("category")
     private Long categoryId;
     @NotNull
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DATE_PARAM_FORMAT_PATTERN)
     private LocalDateTime eventDate;
     private Location location;
     @JsonProperty("paid")
