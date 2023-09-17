b8a14009bc3e42889e4135cccb6ab767325345f0
SergeyZhirov47 <SylarWesker@yandex.ru>
2023-09-17 16:57:23 +0300

    fix: добавлен конструктор по умолчанию для успешной сериализации/десериализации

--- ewm-main-service/src/main/java/ru/practicum/event/model/Location.java
+++ ewm-main-service/src/main/java/ru/practicum/event/model/Location.java
@@ -2,12 +2,14 @@ package ru.practicum.event.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Location {
     private double latitude;
     private double longitude;
